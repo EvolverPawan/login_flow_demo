8a29a67d26dbf38f05f3bd0eb43e60aafe5bd5ee
pawan <evolver.pawan@gmail.com>
2019-03-06 19:05:02 +0530

    Added models file

---
+++ backend/models/Users.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    username: "testPwan",
+    fname: 'Pawan',
+    lname: 'Khaturia',
+    password: '$2a$10$RY/tE6Xl1i.yCArdcivRUOlu7l60yms8hN/diesjJXNJYhze3Vgo.'
+  },
+  {
+    username: 'a@b.com',
+    fname: 'John',
+    lname: 'Deere',
+    password: '$2a$10$mZf9XhQW1jdvrP/ch32e7.n7btWGLrw78xZS9D9SZtIA.Sv9JiRdS'
+  }
+]
+module.exports = {
+  users: users
+}
